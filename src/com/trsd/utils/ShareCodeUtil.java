ad86edd65932e73e42f12f3ca65ebf6ed6bcb623
ThinkPad <ThinkPad@Bohc-CQRDLIF>
2017-08-16 15:35:24 +0800

    增加4位登录验证码函数 Change-Id: I8c83d91c29a3e3ceee17c9dfc0a00fa0fc506c4f

--- src/com/trsd/utils/ShareCodeUtil.java
+++ src/com/trsd/utils/ShareCodeUtil.java
@@ -14,18 +14,25 @@ import java.util.Random;
  */
 public class ShareCodeUtil {
 
+	private static ShareCodeUtil shareCodeUtil = new ShareCodeUtil();
+
+	public static ShareCodeUtil instance() {
+		return shareCodeUtil;
+	}
+
 	/** 自定义进制(0,1没有加入,容易与o,l混淆) */
-	private static final char[] r = new char[] { 'q', 'w', 'e', '8', 'a', 's', '2', 'd', 'z', 'x', '9', 'c', '7', 'p', '5', 'i', 'k', '3', 'm', 'j', 'u', 'f', 'r', '4', 'v', 'y', 'l', 't', 'n', '6',
-			'b', 'g', 'h' };
+	private final char[] r = new char[] { 'q', 'w', 'e', '8', 'a', 's', '2', 'd', 'z', 'x', '9', 'c', '7', 'p', '5', 'i', 'k', '3', 'm', 'j', 'u', 'f', 'r', '4', 'v', 'y', 'l', 't', 'n', '6', 'b', 'g', 'h' };
 
 	/** (不能与自定义进制有重复) */
-	private static final char b = 'o';
+	private final char b = 'o';
 
 	/** 进制长度 */
-	private static final int binLen = r.length;
+	private final int binLen = r.length;
 
 	/** 序列最小长度 */
-	private static final int s = 6;
+	private final int s = 6;
+
+	private Random random = new Random();
 
 	/**
 	 * 根据ID生成六位随机码
@@ -34,7 +41,7 @@ public class ShareCodeUtil {
 	 *            ID
 	 * @return 随机码
 	 */
-	public static String toSerialCode(long id) {
+	public String toSerialCode(long id) {
 		char[] buf = new char[32];
 		int charPos = 32;
 
@@ -60,7 +67,7 @@ public class ShareCodeUtil {
 		return str;
 	}
 
-	public static long codeToId(String code) {
+	public long codeToId(String code) {
 		char chs[] = code.toCharArray();
 		long res = 0L;
 		for (int i = 0; i < chs.length; i++) {
@@ -83,4 +90,14 @@ public class ShareCodeUtil {
 		}
 		return res;
 	}
+
+	/**
+	 * 生成1000－9999的4位随机数来进行登录
+	 * 
+	 * @return
+	 */
+	public int getRandom() {
+		return random.nextInt(8999) + 1000;
+	}
+
 }
