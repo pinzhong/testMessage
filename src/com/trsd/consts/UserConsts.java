806f14935cd465c92d52e695fb08f02c7d68a16a
zhaott <ThinkPad@zhaotingting>
2017-08-21 13:42:39 +0800

    修改用户状态

--- src/com/trsd/consts/UserConsts.java
+++ src/com/trsd/consts/UserConsts.java
@@ -11,8 +11,8 @@ public class UserConsts {
 	 * 用户状态标记常量
 	 */
 	public static final int USER_FLAG_FORBID_ALL = -1 ;//禁止所有操作
-	public static final int USER_FLAG_FORBID_LOAN = 1 ;//禁止借款
-	public static final int USER_FLAG_FORBID_LEND = 2 ;//禁止投资
-	public static final int USER_FLAG_FORBID_WITHDRAW = 3 ;//禁止提现
-	public static final int USER_FLAG_FORBID_LOGIN = 4 ;//禁止登陆
+	public static final int USER_FLAG_FORBID_LOAN = -2 ;//禁止借款
+	public static final int USER_FLAG_FORBID_LEND = -3 ;//禁止投资
+	public static final int USER_FLAG_FORBID_WITHDRAW = -4 ;//禁止提现
+	public static final int USER_FLAG_FORBID_LOGIN = -5 ;//禁止登陆
 }
