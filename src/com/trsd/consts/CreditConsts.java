1a39d5548f21fd4dfc95cf2c091c42a0fba20718
chenhy <chenhy82@qq.com>
2017-08-21 17:23:43 +0800

    修改数据库模型 增加高级认证相关数据表

---
+++ src/com/trsd/consts/CreditConsts.java
@@ -0,0 +1,17 @@
+package com.trsd.consts;
+
+/**
+ * 高级认证相关常量定义
+ * @author chenhy
+ *
+ */
+public class CreditConsts {
+	/**
+	 * 高级认证服务商（寻程）
+	 */
+	public static final String CREDIT_PROVIDER_APISTORE = "apistore";
+	/**
+	 * 高级认证服务商（立木）
+	 */
+	public static final String CREDIT_PROVIDER_LIMU = "limu";
+}
