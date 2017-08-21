5ce8b86e34cb465d2eb4e1a99209da5ee6bfbc82
zhaott <ThinkPad@zhaotingting>
2017-08-21 18:41:39 +0800

    新增常量

---
+++ src/com/trsd/consts/ResultConsts.java
@@ -0,0 +1,18 @@
+package com.trsd.consts;
+
+public class ResultConsts {
+	
+	public static final int RESAULT_SUCCESS = 1 ; //成功
+	public static final int RESAULT_FAIL = 0;
+	
+	public static final String MSG_SUCCESS = "操作成功";
+	public static final String MSG_FAIL = "操作失败";
+	public static final String MSG_UNQUALIFIED_LOAN = "您暂无借款资格";
+	public static final String MSG_UNQUALIFIED_LEND = "您暂无投资资格";
+	public static final String MSG_BALANCE_NOT_ENOUGH = "余额不足，不能投资";
+	public static final String MSG_REAL_NAME_NOT_PASS = "您的实名未通过，不能进行当前操作";
+	public static final String MSG_AVALIABLE_SCORE_NOT_ENOUGH = "您的可用身价不足，不能进行当前操作";
+	public static final String MSG_LOAN_SUCCESS = "恭喜您，您的借款申请已提交成功";
+	public static final String MSG_LOAN_FAIL = "抱歉，您的借款申请提交失败，请您稍后重试";
+
+}
