b016ed71c842f8137d38577e6afc649179e94306
chenhy <chenhy82@qq.com>
2017-08-25 12:00:13 +0800

    修改常量

--- src/com/trsd/consts/CreditConsts.java
+++ src/com/trsd/consts/CreditConsts.java
@@ -6,12 +6,16 @@ package com.trsd.consts;
  *
  */
 public class CreditConsts {
+	public static final int CREDIT_TYPE_BANKCARD = 1;//银行卡三要素认证
+	public static final int CREDIT_TYPE_REALNAME = 2;//身份认证
+	public static final int CREDIT_TYPE_MOBILE = 3;//运营商认证
+	public static final int CREDIT_TYPE_CENTRALBANK = 4;//央行征信
+	public static final int CREDIT_TYPE_TAOBAO = 5;//淘宝
+	public static final int CREDIT_TYPE_JINDONG = 6;//京东	
+	
 	/**
-	 * 高级认证服务商（寻程）
+	 * 高级认证服务商
 	 */
-	public static final String CREDIT_PROVIDER_APISTORE = "apistore";
-	/**
-	 * 高级认证服务商（立木）
-	 */
-	public static final String CREDIT_PROVIDER_LIMU = "limu";
+	public static final String CREDIT_PROVIDER_APISTORE = "apistore"; //寻程
+	public static final String CREDIT_PROVIDER_LIMU = "limu"; 		  //立木
 }
