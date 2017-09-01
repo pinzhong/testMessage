4171e2f90b961fd55970882f5b404f92a10bf0f6
chenhy <chenhy82@qq.com>
2017-09-01 17:33:26 +0800

    增加认证相关公具类

---
+++ src/com/trsd/utils/HttpMethod.java
@@ -0,0 +1,22 @@
+package com.trsd.utils;
+
+/**
+ * Created by lewis on 2016/7/2.
+ */
+public enum HttpMethod {
+    OPTIONS,
+    /** GET. */
+    GET,
+    /** HEAD. */
+    HEAD,
+    /** POST. */
+    POST,
+    /** PUT. */
+    PUT,
+    /** DELETE. */
+    DELETE,
+    /** TRACE. */
+    TRACE,
+    /** PATCH. */
+    PATCH
+}
