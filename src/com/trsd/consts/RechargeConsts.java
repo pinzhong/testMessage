af5f64ee9bc901e5e318ff03370ed4ed191b54fd
main <main@DESKTOP-78D367T>
2017-09-04 15:45:08 +0800

    新增充值状态常量类

---
+++ src/com/trsd/consts/RechargeConsts.java
@@ -0,0 +1,10 @@
+package com.trsd.consts;
+
+public class RechargeConsts {
+	public static final int RECHARGE_INITIAL=0;
+	public static final int RECHARGE_BIND=1;
+	public static final int RECHARGE_SUCCESS=2;
+	
+	public static final int RECHARGE_EXCEPTION_INITIAL=0;
+	public static final int RECHARGE_EXCEDPTION_PROCESSED=1;
+}
