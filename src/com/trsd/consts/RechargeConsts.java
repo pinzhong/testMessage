9ad0f7a8069337a8f2c5caa58cc9224d1eb99db4
main <main@DESKTOP-78D367T>
2017-09-04 17:00:01 +0800

    增加业务类型常量

--- src/com/trsd/consts/RechargeConsts.java
+++ src/com/trsd/consts/RechargeConsts.java
@@ -7,4 +7,7 @@ public class RechargeConsts {
 	
 	public static final int RECHARGE_EXCEPTION_INITIAL=0;
 	public static final int RECHARGE_EXCEDPTION_PROCESSED=1;
+	
+	public static final int BUSINESS_TYPE_NORMAL=0;
+	public static final int BUSINESS_TYPE_FIX=1;
 }
