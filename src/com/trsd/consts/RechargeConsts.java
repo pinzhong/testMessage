00874317475084a5c53dc36c3171d972f9c0885e
main <main@DESKTOP-78D367T>
2017-09-05 10:18:16 +0800

    增加充值常量备注

--- src/com/trsd/consts/RechargeConsts.java
+++ src/com/trsd/consts/RechargeConsts.java
@@ -1,13 +1,17 @@
 package com.trsd.consts;
 
 public class RechargeConsts {
-	public static final int RECHARGE_INITIAL=0;
-	public static final int RECHARGE_BIND=1;
-	public static final int RECHARGE_SUCCESS=2;
+	public static final int RECHARGE_INITIAL=0;//订单初始创建状态
+	public static final int RECHARGE_BIND=1;//已绑定用户
+	public static final int RECHARGE_SUCCESS=2;//成功充值到账务系统
 	
-	public static final int RECHARGE_EXCEPTION_INITIAL=0;
-	public static final int RECHARGE_EXCEDPTION_PROCESSED=1;
+	public static final int RECHARGE_EXCEPTION_INITIAL=0;//异常订单初始状态
+	public static final int RECHARGE_EXCEDPTION_PROCESSED=1;//异常订单已处理
 	
-	public static final int BUSINESS_TYPE_NORMAL=0;
-	public static final int BUSINESS_TYPE_FIX=1;
+	public static final int BUSINESS_TYPE_NORMAL=0;//业务类型普通
+	public static final int BUSINESS_TYPE_FIX=1;//修复型订单
+	
+	public static final String REDIS_RECHAREGE_DAY_COUNTS_KEY="recharge:day:counts:key";//每日次数存储
+	public static final String REDIS_RECHARGE_MINUTE_COUNTS_KEY="recharge:minute:counts:key";//每分钟次数存储
+	
 }
