bd9a93a9305df980df3f492221da2a8c411da94f
main <main@DESKTOP-78D367T>
2017-09-12 16:04:42 +0800

    新增微信绑定状态常量

---
+++ src/com/trsd/consts/WeixinConsts.java
@@ -0,0 +1,12 @@
+package com.trsd.consts;
+
+public class WeixinConsts {
+	/**
+	 * 验证码初始状态
+	 */
+	public static final int VERIFY_CODE_INIT=0;
+	/**
+	 * 验证码绑定成功
+	 */
+	public static final int VERIFY_CODE_BIND_SUCCESS=1;
+}
