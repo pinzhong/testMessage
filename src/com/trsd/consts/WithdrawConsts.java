cc581db3f64f873ad8a7d526dc4b3dfa0dcf0343
main <main@DESKTOP-78D367T>
2017-09-20 17:53:30 +0800

    提现常量类

---
+++ src/com/trsd/consts/WithdrawConsts.java
@@ -0,0 +1,7 @@
+package com.trsd.consts;
+
+public class WithdrawConsts {
+	public static final int RESULT_CARD_STAUTS_NONE = 2;// 没有任何卡绑定信息
+
+	public static final short WHITE_STAUS_SUCCESS = 1;
+}
